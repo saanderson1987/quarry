3acf6b30b2246a22d41e73c766ee3ebe50aff7b3
App Academy Student <saanderson1987@gmail.com>
2017-04-18 17:18:10 -0400

    make ready for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: {format: :json} do
+    resources :benches, only: [:index, :show, :create]
+    resources :reviews, only: [:create]
+    resource :user, only: [:create]
+    resource :session, only: [:create, :destroy, :show]
+    resource :favorites, only: [:create, :destroy]
+  end
+
+  root "static_pages#root"
+
 end
